d84e479429558872422820ed14d97d29966cb8a1
Regina Czech <regina.czech@yahoo.com>
2024-09-04 18:16:47 +0100

    components structure updated

--- pension_tracker/README.md
+++ pension_tracker/README.md
@@ -1,3 +1,9 @@
 # mintago_tech_test
 
 Frontend Developer Technical Test
+
+To Run the App:
+`cd pension_tracker`
+`npm run dev`
+
+To Run the Tests:
---
+++ pension_tracker/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Input from "../../components/InputComponent/Input";
+import Text from "../../components/TextComponent/Text";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Text />
+      <Input />
+    </>
+  );
+};
+
+export default Dashboard;
